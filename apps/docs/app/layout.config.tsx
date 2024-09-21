a285211e8aaac6e0c18722141e848948f1826f4c
Jeremy Bosma <jectaunscripted@gmail.com>
2024-09-21 16:08:46 +0200

    make docs landing

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -9,9 +9,17 @@ import { type HomeLayoutProps } from "fumadocs-ui/home-layout";
  */
 export const baseOptions: HomeLayoutProps = {
   nav: {
-    title: "My App",
+    title: <><svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg"><rect id="Rectangle 14" x="16.6777" y="-1" width="25" height="25" rx="2.5" transform="rotate(45 16.6777 -1)" fill="white" /></svg><p>Lapse</p></>
   },
   links: [
+    {
+      text: "Pricing",
+      url: "https://guide.lapse.host/integrations",
+    },
+    {
+      text: "Feedback",
+      url: "https://lapse.canny.io/",
+    },
     {
       text: "Documentation",
       url: "/docs",
